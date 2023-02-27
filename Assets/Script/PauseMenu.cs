ac4e41bc981c7af64364ec11b9bd75ef32fa40d1
Anping6630 <anping6630@gmail.com>
2023-02-28 06:14:57 +0800

    Scene transition / Game Pause

---
+++ Assets/Script/PauseMenu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField]
+    [Header("暫停選單")]
+    private GameObject menu;
+
+    private bool isGamePause;
+
+    void Start()
+    {
+         GameResume();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (isGamePause)
+            {
+                GameResume();
+            }
+            else
+            {
+                GamePause();
+            }
+        }
+    }
+
+    public void GameResume()
+    {
+        isGamePause = false;
+        menu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void GamePause()
+    {
+        isGamePause = true;
+        menu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+}

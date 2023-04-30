284ad729187e04340771d7efd020c891a08b3a53
Anping6630 <anping6630@gmail.com>
2023-05-01 04:56:12 +0800

    Arduino 升級

--- Assets/Script/Arduino/ArduinoRead.cs
+++ Assets/Script/Arduino/ArduinoRead.cs
@@ -5,19 +5,18 @@ using System.IO.Ports;
 
 public class ArduinoRead : MonoBehaviour
 {
-    [Header("ArduinoPort")]
-    public string portName;
-    [Header("BaudRate")]
-    public int baud;
-
-    SerialPort sp;
-
+    [Header("ArduinoPort"),SerializeField]
+    string portName;
     public float valueL;
+    public float oldL;
     public float valueR;
+    SerialPort sp;
+    [SerializeField]
+    CalibrationData calibrationData;
 
     void Start()
     {
-        sp = new SerialPort(portName,baud);
+        sp = new SerialPort(portName,9600);
         try
         {
             sp.Open();
@@ -36,12 +35,18 @@ public class ArduinoRead : MonoBehaviour
             if (sp.IsOpen)
             {
                 string value = sp.ReadExisting();
-                print(value);
                 if (value != "")
                 {
-                    string[] sArray = value.Split("~");
-                    float.TryParse(sArray[0], out valueL);
-                    float.TryParse(sArray[1], out valueR);
+                    //string[] sArray = value.Split("~");
+                    //float.TryParse(sArray[0], out valueL);
+                    //float.TryParse(sArray[1], out valueR);
+                    float.TryParse(value, out oldL);
+                    if(Remap(oldL)>0&& Remap(oldL) < 1)
+                    {
+                        valueL = Remap(oldL);
+                    }
+                    //Remap(valueR);
+                    print(valueL);
                 }
             }
         }
@@ -51,8 +56,40 @@ public class ArduinoRead : MonoBehaviour
         }
     }
 
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            PlayerPrefs.SetFloat("topValue", oldL) ;
+            print("紀錄高位置" + oldL);
+        }
+        if (Input.GetKeyDown("w"))
+        {
+            PlayerPrefs.SetFloat("bottomValue", oldL);
+            print("紀錄低位置" + oldL);
+        }
+    }
+
+    float Remap(float x)
+    {
+        return (x-PlayerPrefs.GetFloat("topValue"))/(PlayerPrefs.GetFloat("bottomValue")- PlayerPrefs.GetFloat("topValue"));
+    }
+
     void OnApplicationQuit()
     {
         sp.Close();
+    }
+
+    void CalibrationSave()
+    {
+        PlayerPrefs.SetFloat("topValue", calibrationData.topValue);
+        PlayerPrefs.SetFloat("bottomValue", calibrationData.bottomValue);
+    }
+
+    [System.Serializable]
+    public class CalibrationData
+    {
+        public float topValue;
+        public float bottomValue;
     }
 }
